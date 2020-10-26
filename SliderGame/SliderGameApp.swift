26ca6b1d18998d2ba2861cc459d77976ed99d546
anirakanna <evgenia.shipova@gmail.com>
2020-10-26 21:52:29 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Evgenia Shipova on 26.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
